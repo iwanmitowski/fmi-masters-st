f234200c0a6d958f535eccd5d1faba81dd56d18c
Ivan Mitovski <imitovski@nemetschek.bg>
2024-11-16 21:24:52 +0200

    HW p1 - Init project

---
+++ projects/p1-rent-a-car/src/test/java/com/example/p1_rent_a_car/P1RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p1_rent_a_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
