a7370aadc2039408a67d94a16079b2d6bfe8957c
Ivan Mitovski <imitovski@nemetschek.bg>
2024-12-24 18:44:35 +0200

    HW pfinal - Initial commit

---
+++ projects/pfinal-chat-mat/src/main/java/com/example/chat_mat/ChatMatApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_mat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMatApplication.class, args);
+	}
+
+}
---
+++ projects/pfinal-chat-mat/src/test/java/com/example/chat_mat/ChatMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_mat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
