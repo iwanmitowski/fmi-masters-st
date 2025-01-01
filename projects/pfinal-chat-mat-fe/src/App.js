985a2da8cfd1b23593b2c36934f4cb2528851fba
Ivan Mitovski <imitovski@nemetschek.bg>
2025-01-01 12:31:08 +0200

    HW pfinal - ReactJS initial commit

---
+++ projects/pfinal-chat-mat-fe/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./components/shared/Main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main>
+        <h1>hi</h1>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
